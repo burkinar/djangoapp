8c1ade2ef8039c761dced0013ccd57b0263b8360
samandoulougou Stephane <burkinar@gmail.com>
2025-06-23 02:46:14 +0000

    static

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path("tips/", include("wepynaire.tips.urls")),
     path("users/", include("wepynaire.users.urls")),
 ]
-+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
