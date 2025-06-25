b084404fc51f364af093acb2a74925ea449166fd
samandoulougou Stephane <burkinar@gmail.com>
2025-06-25 18:25:01 +0000

    manage.py header

--- config/settings.py
+++ config/settings.py
@@ -10,15 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-
-import environ
-
-env = environ.Env()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+from config import BASE_DIR, env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
